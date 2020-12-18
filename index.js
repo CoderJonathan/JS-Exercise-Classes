023bf098cb1ffc30b14bb23b8b2d9aaebec04a18
Jonathan Garcia <70542639+CoderJonathan@users.noreply.github.com>
2020-12-17 19:06:05 -0800

    Almost finished just stuck on task 5 and 6

--- index.js
+++ index.js
@@ -10,93 +10,155 @@
 
 // EXAMPLE SOLUTION CODE:
 class Airplane {
-    constructor(name) {
-      this.name = name;
-      this.isFlying = false;
-    }
-    takeOff() {
-      this.isFlying = true;
-    }
-    land() {
-      this.isFlying = false;
-    }
+  constructor(name) {
+    this.name = name;
+    this.isFlying = false;
   }
-  
-  /*
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  */
-  
-  /*
-    TASK 1
-      - Write a Person class whose constructor initializes `name` and `age` from arguments.
-      - All instances of Person should also initialize with an empty `stomach` array.
-      - Give instances of Person the ability to `.eat("someFood")`:
-          + When eating an edible, it should be pushed into the `stomach`.
-          + The `eat` method should have no effect if there are 10 items in the `stomach`.
-      - Give instances of Person the ability to `.poop()`:
-          + When an instance poops, its `stomach` should empty.
-      - Give instances of Person a method `.toString()`:
-          + It should return a string with `name` and `age`. Example: "Mary, 50"
-  */
-  
- class Person {
-    
+  takeOff() {
+    this.isFlying = true;
   }
-  
-  /*
-    TASK 2
-      - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
-      - All instances built with Car:
-          + should initialize with a `tank` at 0
-          + should initialize with an `odometer` at 0
-      - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-      - Give cars ability to `.drive(distance)`. The distance driven:
-          + Should cause the `odometer` to go up.
-          + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-      - A car which runs out of `fuel` while driving can't drive any more distance:
-          + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
-  */
-  
- class Car {
-    
+  land() {
+    this.isFlying = false;
   }
-  
-  /*
-    TASK 3
-      - Write a Lambdasian class.
-      - Its constructor takes a single argument - an object with the following keys:
-          + name
-          + age
-          + location
-      - Its constructor should initialize `name`, `age` and `location` properties on the instance.
-      - Instances of Lambdasian should be able to `.speak()`:
-          + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
-          + {name} and {location} of course come from the instance's own properties.
-  */
- class Lambdasian {
-    
-  }
-  
-  /*
-    TASK 4
-      - Write an Instructor class extending Lambdasian.
-      - Its constructor takes a single argument - an object with the following keys:
-          + All the keys used to initialize instances of Lambdasian.
-          + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
-          + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
-          + `catchPhrase`: i.e. `Don't forget the homies`.
-      - The constructor calls the parent constructor passing it what it needs.
-      - The constructor should also initialize `specialty`, `favLanguage` and `catchPhrase` properties on the instance.
-      - Instructor instances have the following methods:
-          + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-          + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
-  */
- class Instructor {
+}
 
- }
-  /*
+/*
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+*/
+
+/*
+  TASK 1
+    - Write a Person class whose constructor initializes `name` and `age` from arguments.
+    - All instances of Person should also initialize with an empty `stomach` array.
+    - Give instances of Person the ability to `.eat("someFood")`:
+        + When eating an edible, it should be pushed into the `stomach`.
+        + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - Give instances of Person the ability to `.poop()`:
+        + When an instance poops, its `stomach` should empty.
+    - Give instances of Person a method `.toString()`:
+        + It should return a string with `name` and `age`. Example: "Mary, 50"
+*/
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = []; 
+}
+
+eat(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+}
+
+poop() {
+  this.stomach.splice(0);
+  return this.stomach;
+}
+
+toString() {
+  return `${this.name}, ${this.age}`;
+  }
+}
+
+/*
+  TASK 2
+    - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
+    - All instances built with Car:
+        + should initialize with a `tank` at 0
+        + should initialize with an `odometer` at 0
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - Give cars ability to `.drive(distance)`. The distance driven:
+        + Should cause the `odometer` to go up.
+        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - A car which runs out of `fuel` while driving can't drive any more distance:
+        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+*/
+
+class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons) {
+    return (this.tank += gallons);
+  }
+
+  drive(distance) {
+    if (this.tank - distance / this.milesPerGallon > 0) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}!`;
+    }
+  }
+}
+
+/*
+  TASK 3
+    - Write a Lambdasian class.
+    - Its constructor takes a single argument - an object with the following keys:
+        + name
+        + age
+        + location
+    - Its constructor should initialize `name`, `age` and `location` properties on the instance.
+    - Instances of Lambdasian should be able to `.speak()`:
+        + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
+        + {name} and {location} of course come from the instance's own properties.
+*/
+class Lambdasian {
+  constructor(lambda) {
+    (this.name = lambda.name),
+      (this.age = lambda.age),
+      (this.location = lambda.location);
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+/*
+  TASK 4
+    - Write an Instructor class extending Lambdasian.
+    - Its constructor takes a single argument - an object with the following keys:
+        + All the keys used to initialize instances of Lambdasian.
+        + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
+        + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
+        + `catchPhrase`: i.e. `Don't forget the homies`.
+    - The constructor calls the parent constructor passing it what it needs.
+    - The constructor should also initialize `specialty`, `favLanguage` and `catchPhrase` properties on the instance.
+    - Instructor instances have the following methods:
+        + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+        + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
+*/
+class Instructor extends Lambdasian {
+
+  constructor(instrObj) {
+    super(instrObj);
+    (this.specialty = instrObj.specialty),
+      (this.favLanguage = instrObj.favLanguage),
+      (this.catchPhrase = instrObj.catchPhrase);
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}'`;
+  }
+}
+
+/*
     TASK 5
       - Write a Student class extending Lambdasian.
       - Its constructor takes a single argument -  an object with the following keys:
